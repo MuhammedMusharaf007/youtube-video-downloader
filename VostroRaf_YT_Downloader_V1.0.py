6fb7aa20595a521abbc7359858f3f47c77d24963
MuhammedMusharaf007 <mohammedmusharafnilambur@gmail.com>
2022-07-03 23:29:46 +0530

    Added the download and rerun functions

--- VostroRaf_YT_Downloader_V1.0.py
+++ VostroRaf_YT_Downloader_V1.0.py
@@ -26,3 +26,30 @@ y_val = int(window_height - app_height) / 2
 
 # passing the set values as the app geometry 
 app_base.geometry(f"{app_width}x{app_height}+{x_val}+{y_val}")
+
+# initialising an empty dictionary 
+youtube_dl_dict = {}
+
+# defining the download function 
+def download():
+    video_link = Vostro.get()
+    link_strip_txt = video_link.strip()
+    with youtube_dl.YoutubeDL(youtube_dl_dict) as downloads:
+        downloads.download([link_strip_txt])
+
+# defining a rerun callback function incase any error or event occurs
+def rerun(instance):
+    if Vostro.get() == "Paste the video link here.":
+        Vostro.delete(0, "end")
+
+# Assigning the head label
+head_label = Label(app_base, text = 'High Quality Youtube video downloader from VostroRaf', fg = "white", bg = "blue")
+
+# defining the entry point for the app
+Vostro = Entry(app_base)
+Vostro.config(font = ('arial', 15), width = 30)
+Vostro.insert(END, "Paste the video link here.")
+Vostro.bind("<Button-1>", rerun)
+Vostro.place(x = 25, y = 120)
+button = Button(app_base, text = "Get it!", bg = "darkblue", fg = "white", font = ('arial', 18))
+app_base.mainloop()
